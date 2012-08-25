8edcf9b8cc0852b4cc131399da346e5c123e57eb
spinfoltd <spinfoltd@gmail.com>
2012-08-25 11:22:50 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Baton::Application.config.secret_token = 'ccd437ca313f487cb849843a48247ad2e43a7f22a8f33d7ee0c802afc869dc0d1ee44b9d4c121b500341f1e85d2e86c12568fb8143e222abedcbd72f7459f72e'
